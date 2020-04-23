e4b3358b9f36edbd2fc8c12bf985ddc3532dab7d
Tqualizer <52141495+Tqualizer@users.noreply.github.com>
2020-04-23 01:21:52 +0100

    Update opencv_group_detection.py

--- opencv_group_detection.py
+++ opencv_group_detection.py
@@ -2,16 +2,16 @@
 #> Author: Tahir Mahmood
 #> Date: 15/4/20
 #> Description: This code draws upon standard examples in object detection using opencv to: 
-# Uses a TensorFlow classifier to perform object detection and counting.
-# Identify and count and keep a tally based on only specified class(es) returned by the tensorflow model. 
-# Log whenever a specified number of a specified class is returned and also capture an image of each example.
+#>> Use a TensorFlow classifier to perform object detection and counting.
+#>> Identify and count and keep a tally based on only specified class(es) returned by the tensorflow model. 
+#>> Log whenever a specified number of a specified class is returned and also capture an image of each example.
+
 #> Suggested usage: Detecting groups of people not observing social distancing whilst walking through a narrow line of sight.
 #> Note: This can be futher enhanced with object tracking to avoid overcounting of very slow moving or stationary groups.
 
 ## Some of the code is copied from:
 ## https://github.com/EdjeElectronics/TensorFlow-Object-Detection-on-the-Raspberry-Pi/blob/master/Object_detection_picamera.py
-## And draws from ideas in:
-## https://github.com/vineeth2628/Object-counting-using-tensorflow-on-raspberry-pi
+## And draws from ideas in: https://github.com/vineeth2628/Object-counting-using-tensorflow-on-raspberry-pi
 
 # Import packages
 import os
@@ -26,6 +26,7 @@ import sys
 import time
 import csv
 
+######## BOILERPLATE CODE #######
 # Set up camera constants
 #IM_WIDTH = 1280
 #IM_HEIGHT = 720
@@ -144,6 +145,7 @@ def group_counting():
             line_thickness=6,
             min_score_thresh=0.50)
         
+        ####### OBJECT SELECTION AND COUNTING CODE STARTS HERE #######
         # pulling raw output from object detection. Creates a list of dicts 
         # with details of each of the objects meeting the threshold in a given frame.
         Validobj = [category_index.get(value) for index, value in enumerate (classes[0]) if scores [0,index]>0.5]
