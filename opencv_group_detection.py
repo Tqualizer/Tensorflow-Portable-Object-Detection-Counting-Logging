494645b6738823762f4ec5e38619d015df96d3c1
Tqualizer <52141495+Tqualizer@users.noreply.github.com>
2020-04-23 01:30:07 +0100

    Update opencv_group_detection.py

--- opencv_group_detection.py
+++ opencv_group_detection.py
@@ -2,14 +2,14 @@
 #> Author: Tahir Mahmood
 #> Date: 15/4/20
 #> Description: This code draws upon standard examples in object detection using opencv to: 
-#>> Use a TensorFlow classifier to perform object detection and counting.
+#>> Use a TensorFlow classifier to perform live object detection viewing and counting.
 #>> Identify and count and keep a tally based on only specified class(es) returned by the tensorflow model. 
 #>> Log whenever a specified number of a specified class is returned and also capture an image of each example.
 
 #> Suggested usage: Detecting groups of people not observing social distancing whilst walking through a narrow line of sight.
 #> Note: This can be futher enhanced with object tracking to avoid overcounting of very slow moving or stationary groups.
 
-## Some of the code is copied from:
+## ## The boilerplate part of the code is copied from:
 ## https://github.com/EdjeElectronics/TensorFlow-Object-Detection-on-the-Raspberry-Pi/blob/master/Object_detection_picamera.py
 ## And draws from ideas in: https://github.com/vineeth2628/Object-counting-using-tensorflow-on-raspberry-pi
 
